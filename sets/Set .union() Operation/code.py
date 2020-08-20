cb1bab2a211859e08503b61947a4e2b06b37366f
CodeWithEhtisham <shamikhan0347@gmail.com>
2020-08-20 20:55:08 +0500

    sets problem complete

---
+++ sets/Set .union() Operation/code.py
@@ -0,0 +1,16 @@
+n=int(input())
+N=set(map(int,input().split(" ")))
+b=int(input())
+B=set(map(int,input().split(" ")))
+
+print(len(N.union(B)))
+
+
+
+
+
+
+# 9
+# 1 2 3 4 5 6 7 8 9
+# 9
+# 10 1 2 3 11 21 55 6 8
