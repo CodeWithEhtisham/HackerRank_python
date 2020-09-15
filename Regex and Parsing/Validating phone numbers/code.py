135650047b404f819fb636578392dbb8c09f2b0c
CodeWithEhtisham <shamikhan0347@gmail.com>
2020-09-16 01:19:33 +0500

    problem 8 regex update

--- Regex and Parsing/Validating phone numbers/code.py
+++ Regex and Parsing/Validating phone numbers/code.py
@@ -2,6 +2,6 @@
 # n=int(input())
 # regex=r'(7|8|9)[1-9]{10}'
 # for i in range(n):
-#     print(re.search(regex,input()))
+#     print(re.match(regex,input()))
 import re
 [print('YES' if re.match(r'[789]\d{9}$',input()) else 'NO') for _ in range(int(input()))]
