16451c659809c8d8f045dfe64f396d660fb0f282
CodeWithEhtisham <shamikhan0347@gmail.com>
2020-09-26 02:06:08 +0500

    xml part 1

---
+++ XML/XML 1 - Find the Score/code.py
@@ -0,0 +1,17 @@
+import sys
+import xml.etree.ElementTree as etree
+
+def get_attr_number(node):
+    
+    count=0
+    for i in node.iter():
+        count+=len(i.attrib)
+    return count
+    # your code goes here
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
