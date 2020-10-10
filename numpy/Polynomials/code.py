9acb43fae77708de053ad0b444b3d44ead2f4d1e
CodeWithEhtisham <shamikhan0347@gmail.com>
2020-10-11 00:29:26 +0500

    polyval

--- numpy/Polynomials/code.py
+++ numpy/Polynomials/code.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-n = list(map(float,input().split()));
-m = input();
-print(np.polyval(n,int(m)));
+n = list(map(float,input().split()))
+m = input()
+print(np.polyval(n,int(m)))
